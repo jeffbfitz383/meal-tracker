6ad919c58b3969ac7f0fb876a5b292224e3fd297
Jeffrey <jefbfitzp33@outlook.com>
2024-04-12 17:33:55 -0600

    add blank user table

--- server/models.py
+++ server/models.py
@@ -6,5 +6,9 @@ from flask_bcrypt import Bcrypt
 from sqlalchemy.ext.hybrid import hybrid_property
 bcrypt = Bcrypt()
 
+class User(db.Model):
+    __tablename__= "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
 
 # Models go here!
