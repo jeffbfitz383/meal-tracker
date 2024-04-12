fcbfd1cfd863d9386c4a1f72e84726e3b050c679
Jeffrey <jefbfitzp33@outlook.com>
2024-04-12 17:28:19 -0600

    add necessary imports to models.db

--- server/models.py
+++ server/models.py
@@ -1,6 +1,10 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from config import *
+from flask_bcrypt import Bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
+bcrypt = Bcrypt()
 
-from config import db
 
 # Models go here!
