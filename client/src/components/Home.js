65655b7fd5ffc8dbbcc064d196c530a67a35fe90
Jeffrey <jefbfitzp33@outlook.com>
2024-04-12 20:51:02 -0600

    Add home.js component

---
+++ client/src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link, useParams, useHistory} from "react-router-dom";
+// 
+
+
+function Home() {
+
+
+
+    return (
+        <div>
+            //<button  class="button-63" role="button" onClick={handleNavtoSignup}>Signup</button><p></p>
+           // <button class="button-63" role="button" onClick={handleNavtoLoggin}>Loggin</button>
+            <div class ="Title">
+                Highlight Video Maker
+            </div>
+
+          
+
+        </div>
+    );
+};
+
+export default Home;
